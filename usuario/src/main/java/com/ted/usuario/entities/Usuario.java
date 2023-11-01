868e9bfe30617f342fe5d54caa0479fef2b93b80
ThiagoMonts <thiagohonorato@hotmail.com>
2023-11-01 11:52:58 -0300

    Update Usuario.java

--- usuario/src/main/java/com/ted/usuario/entities/Usuario.java
+++ usuario/src/main/java/com/ted/usuario/entities/Usuario.java
@@ -15,4 +15,44 @@ public class Usuario {
     private String password;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
